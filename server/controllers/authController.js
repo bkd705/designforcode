573a0ee1c4f9d31aa8e26548e9d1d3e017ab965b
Zach Shames <zlshames@gmail.com>
2017-03-07 12:15:44 -0500

    Simplified JResponse. Simplified some stuff in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,35 +2,32 @@ import db from '../config/db'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import JRes from '../util/JResponse'
+import Helpers from '../util/Helpers'
 
 const users = db.users
 
 export default class AuthController {
-  
+
   static * login(next) {
     const user = this.request.body
 
     const result = yield users.findOne({
-      where: {
-        username: user.username
-      }
+      where: { username: user.username }
     })
     .then(foundUser => {
-      if(!foundUser) {
+      if (!foundUser) {
         return JRes.failure('No user found with that username!')
       }
-      if(!bcrypt.compareSync(user.password, foundUser.password)) {
+
+      if (!bcrypt.compareSync(user.password, foundUser.password)) {
         return JRes.failure('Password is incorrect.')
       }
 
-      const userMin = {
-        id: foundUser.id,
-        username: foundUser.username,
-        email: foundUser.email
-      }
-
       const token = jwt.sign(userMin, process.env.JWT_SECRET)
-      return JRes.success('Successfully created new user!', { user: userMin, token: token })
+      return JRes.success('Successfully created new user!', {
+        user: Helpers.transformObj(user.dataValues, ['id', 'username', 'email']),
+        token: token
+      })
     })
     .catch(err => {
       return JRes.failure(err)
