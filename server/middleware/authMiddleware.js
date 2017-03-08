4ee2883261ea6ab75e281ca00398a96c8ad50b7a
bkd705 <brennenkdenomme@gmail.com>
2017-03-08 15:27:26 -0500

    Re-factored authMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,28 +1,52 @@
 import jwt from 'jsonwebtoken'
 import db from '../config/db'
+import JRes from '../util/JResponse'
 
 const users = db.users
 
 export default function * (next) {
+  if (!this.headers.authorization) {
+    this.body = JRes.failure('No authorization header provided')
+    return
+  }
+
   const token = this.headers.authorization.split(' ')[1]
+
+  if (!token) {
+    this.body = JRes.failure('No token in authorization header provided')
+    return
+  }
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
 
-    users.findOne({
+    const result = users.findOne({
       where: {
         id: payload.id
       }
     })
     .then(user => {
       if (!user) {
-        this.state.user = null
+        this.status = 500
+        return JRes.failure('No user found')
       }
 
       this.state.user = user
     })
+
+    if (!result.success) this.status = 400
+    this.body = result
   } catch (e) {
-    this.state.user = null
+    if (e.name === 'TokenExpiredError') {
+      this.body = JRes.failure('Token provided has expired', e)
+      return
+    } else if (e.name === 'JsonWebTokenError' && e.message === 'invalid signature') {
+      this.body = JRes.failure('Not a valid token', e)
+      return
+    } else {
+      this.body = JRes.failure('An unexpected error occured', e)
+      return
+    }
   }
 
   yield next
