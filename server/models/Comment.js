cf6006e742c406c90f8c7f701bc01a8fc130e5bb
Zach Shames <zlshames@gmail.com>
2017-03-08 18:51:50 -0500

    Created migration and model for comments, and added JWT expiration

---
+++ server/models/Comment.js
@@ -0,0 +1,40 @@
+export default (sequelize, DataTypes) => {
+  const Comment = sequelize.define('comments', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    post_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'posts',
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      required: true
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updated_at: DataTypes.DATE,
+    deleted_at: DataTypes.DATE
+  }, {
+    paranoid: true,
+    underscored: true
+  })
+
+  return Comment
+}
