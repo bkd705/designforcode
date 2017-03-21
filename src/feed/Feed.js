e59645e2315d8ded730b97fb2777d84efa70cdda
bkd705 <brennenkdenomme@gmail.com>
2017-03-21 19:58:38 -0400

    Added level bar for feed options. Added basic filter functionality

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -8,7 +8,9 @@ class Feed extends React.Component {
     super(props)
 
     this.state = {
-      posts: []
+      posts: [],
+      filteredPosts: [],
+      typeFilter: 'all',
     }
   }
 
@@ -16,7 +18,8 @@ class Feed extends React.Component {
     Api.fetchPosts()
       .then(res => {
         this.setState({
-          posts: res.data.posts
+          posts: res.data.posts,
+          filteredPosts: res.data.posts
         })
       })
       .catch(err => {
@@ -24,13 +27,57 @@ class Feed extends React.Component {
       })
   }
 
+  changeFilter = (e, filter) => {
+    e.preventDefault()
+
+    if(this.setState.typeFilter !== filter) {
+      const newFilteredPosts = filter !== 'all' ? this.state.posts.filter(x => x.type === filter) : this.state.posts
+
+      this.setState({
+        typeFilter: filter,
+        filteredPosts: newFilteredPosts
+      })
+    }
+  }
+
   render() {
-    console.log(this.state)
+    const { typeFilter } = this.state
     return (
-      <div className="container--feed">
-        {this.state.posts.map(post => {
-          return <Post post={post} key={post.id} />
-        })}
+      <div className="container container--feed">
+        <nav className="level">
+          <div className="level-left">
+            <div className="level-item">
+              <p className="subtitle is-5">
+                <strong>{this.state.filteredPosts.length}</strong> posts
+              </p>
+            </div>
+            <div className="level-item">
+              <div className="field has-addons">
+                <p className="control">
+                  <input className="input" type="text" placeholder="Find a post" />
+                </p>
+                <p className="control">
+                  <button className="button">
+                    Search
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="level-right">
+            <p className="level-item"><a onClick={(e) => this.changeFilter(e, 'all')}>{ typeFilter === 'all' ? <strong>All posts</strong> : 'All posts' }</a></p>
+            <p className="level-item">Looking for:</p>
+            <p className="level-item"><a onClick={(e) => this.changeFilter(e, 'design')}>{ typeFilter === 'design' ? <strong>Design</strong> : 'Design' }</a></p>
+            <p className="level-item"><a onClick={(e) => this.changeFilter(e, 'code')}>{ typeFilter === 'code' ? <strong>Code</strong> : 'Code' }</a></p>
+            <p className="level-item"><a className="button is-success">Create Yours</a></p>
+          </div>
+        </nav>
+        <div className="feed">
+          {this.state.filteredPosts.map(post => {
+            return <Post post={post} key={post.id} />
+          })}
+        </div>
       </div>
     )
   }
