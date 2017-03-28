e3ed533c4ba1f54320c9296e2d760592a0aab321
bkd705 <brennenkdenomme@gmail.com>
2017-03-27 22:47:28 -0400

    Made notfound page full height and cleaned up inline styles

--- src/not-found/NotFound.js
+++ src/not-found/NotFound.js
@@ -1,32 +1,21 @@
 import React from 'react'
 import Image from './404-image.png'
+import './notfound.css'
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div style={styles.halfWindow}>
-          <img
-            style={{width: '30%'}}
-            src={Image} />
+      <div className="hero is-info is-bold is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <img className="hero--image" src={Image} />
 
-          <h1 style={{color: 'whitesmoke', fontSize: '3em'}}>404 Page Not Found</h1>
-          <p style={{color: 'whitesmoke'}}>This is not the page you are looking for...</p>
+            <h1 className="title">404 Page Not Found</h1>
+            <h2 className="subtitle">This is not the page you are looking for...</h2>
+          </div>
         </div>
       </div>
     )
-  }
-}
-
-const styles = {
-  halfWindow: {
-    height: '400px',
-    marginTop: '-1px',
-    backgroundImage: 'linear-gradient(90deg, #1577c6 0, #3273dc 71%, #4366e5 100%)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
   }
 }
 
