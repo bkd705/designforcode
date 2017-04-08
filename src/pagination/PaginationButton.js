85203a17b80e42c576eff8e99ec22cd7f3b78ec8
Zach Shames <zlshames@gmail.com>
2017-04-08 19:16:13 -0400

    Made pagination better

--- src/pagination/PaginationButton.js
+++ src/pagination/PaginationButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './pagination.css'
 
 const PaginationButton = ({ value, onClick, active }) => {
   return (
     <li onClick={onClick}>
-      { active 
-        ? <a className="pagination-link is-current">{value}</a> 
+      { active
+        ? <a className="pagination-link is-current">{value}</a>
         : <a className="pagination-link">{value}</a>  }
     </li>
   )
--- src/pagination/PaginationList.js
+++ src/pagination/PaginationList.js
@@ -1,26 +1,26 @@
 import React, { PropTypes as P } from 'react'
 import PaginationButton from './PaginationButton'
 
+import './pagination.css'
+
 const PaginationList = ({ pages, currentPage, changePage }) => {
 
   let buttons = []
   if(pages > 0) {
     for(let i = 1; i < pages + 1; i++) {
-      i === currentPage 
-      ? buttons.push(<PaginationButton key={i} value={i} onClick={(e) => changePage(e, i)} active />) 
+      i === currentPage
+      ? buttons.push(<PaginationButton key={i} value={i} onClick={(e) => changePage(e, i)} active />)
       : buttons.push(<PaginationButton key={i} value={i} onClick={(e) => changePage(e, i)} />)
     }
   }
 
   return (
-    <nav className="pagination is-centered">
-      <a onClick={(e) => changePage(e, currentPage - 1)} className="pagination-previous">Previous</a>
-      <a onClick={(e) => changePage(e, currentPage + 1)} className="pagination-next">Next</a>
-
+    <nav className="pagination">
+      <a onClick={(e) => changePage(e, currentPage - 1)} className="pagination-previous" title="This is the first page">&laquo;</a>
       <ul className="pagination-list">
-        
-        {pages > 0 && buttons} 
+        {pages > 0 && buttons}
       </ul>
+      <a onClick={(e) => changePage(e, currentPage + 1)} className="pagination-next">&raquo;</a>
     </nav>
   )
 }
